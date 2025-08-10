71f3bdf8740ba94a9ccb3daed457316ffcc9c719
kimchj <wnmh123@naver.com>
2025-08-10 15:30:29 +0900

    first commit for fds evaluation service demo

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.4.8"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "kr.co.kimga"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://packages.confluent.io/maven/") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.kafka:spring-kafka")
+	implementation("org.apache.kafka:kafka-clients:3.7.0")
+	implementation("io.confluent:kafka-avro-serializer:7.6.1")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("com.googlecode.aviator:aviator:5.4.3") // 고속 expression evaluator
+
+	implementation("io.micrometer:micrometer-registry-prometheus")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	}
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/kotlin/kr/co/kimga/fdsevaldemo/FdsevaldemoApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.kimga.fdsevaldemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FdsevaldemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<FdsevaldemoApplication>(*args)
+}
