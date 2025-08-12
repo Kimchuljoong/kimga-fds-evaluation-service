0f8aeb9d89b17a54a4500ebda5fb592455ea8cdf
kimchj <wnmh123@naver.com>
2025-08-13 00:06:48 +0900

    making KafkaStreamConfig

--- src/main/kotlin/kr/co/kimga/fdsevaldemo/config/KafkaStreamConfig.kt
+++ src/main/kotlin/kr/co/kimga/fdsevaldemo/config/KafkaStreamConfig.kt
@@ -1,14 +1,18 @@
 package kr.co.kimga.fdsevaldemo.config
 
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde
+import org.apache.avro.specific.SpecificRecord
 import org.apache.kafka.common.serialization.Serde
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class KafkaStreamConfig {
-
-
-
-
-
+class KafkaStreamConfig(
+    @Value("\${spring.kafka.schema-registry-url}") private val schemaRegistryUrl: String
+) {
+    fun <T: SpecificRecord> specificAvroSerde(): Serde<T> {
+        val serde = SpecificAvroSerde<T>()
+        serde.configure(mapOf("schema.registry.url" to schemaRegistryUrl), false)
+        return serde
+    }
 }
